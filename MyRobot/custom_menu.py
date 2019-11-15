aa4825bcd0a7ba0df4794857d20e653601e6a455
DongXuezhen <xzdong@mail.ustc.edu.cn>
2019-11-15 10:45:15 +0800

    Commit for test dxz

---
+++ MyRobot/custom_menu.py
@@ -0,0 +1,27 @@
+from werobot import WeRoBot
+import mcdb.config as CONFIG
+
+
+robot = WeRoBot(token="tiandaotiyu")
+robot.config['HOST'] = '0.0.0.0'
+robot.config['PORT'] = 8000
+robot.config["APP_ID"] = CONFIG.app_id
+robot.config["APP_SECRET"] = CONFIG.app_secret
+
+client = robot.client
+
+client.create_menu({
+    "button":[
+        {
+            "type": "view",
+            "name": "进入主页",
+            "url": "http://xuezhen.natapp1.cc/r_oauth"
+        },
+        {
+            "type": "view",
+            "name": "老师登录",
+            "url": "http://xuezhen.natapp1.cc/prelogin_teacher"
+        }
+    ]
+})
+
